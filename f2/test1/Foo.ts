124f26a030f4575d04c8791b0bed5b2ca0a9fcc4
ItaiApiiro <90134061+ItaiApiiro@users.noreply.github.com>
2021-10-24 12:14:36 +0300

    Update and rename Foo.java to Foo.ts

---
+++ f2/test1/Foo.ts
@@ -0,0 +1,27 @@
+
+
+  @Get()
+  @Bind(Query())
+  findAll(query) {
+    console.log(query);
+    return `This action returns all cats (limit: ${query.limit} items)`;
+  }
+
+  @Get(':id')
+  @Bind(Param('id'))
+  findOne(id) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  @Bind(Param('id'), Body())
+  update(id, updateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  @Bind(Param('id'))
+  remove(id) {
+    return `This action removes a #${id} cat`;
+  }
+}
